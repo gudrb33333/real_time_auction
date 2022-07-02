3a4db221c165823c54acafe24883fec49cbdc2b2
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-07-02 18:28:54 +0900

    health체크 url 추가

--- lib/real_time_auction_web/controllers/page_controller.ex
+++ lib/real_time_auction_web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule RealTimeAuctionWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def health(conn, _params) do
+    send_resp(conn, 200, "ok")
+  end
 end
